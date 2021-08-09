fd8ef9d28204fd1e060187d44ab0025073ff7b1a
vickykimani <victorianightingale70@gmail.com>
2021-08-09 09:22:28 +0300

    welcome page after signup(js)

---
+++ src/welcomeafterregister.js
@@ -0,0 +1,123 @@
+import React from 'react'
+
+export default function welcomeafterregister() {
+    return (
+        <body class="sub_page">
+  <div class="hero_area">
+    
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <h3>
+              NisaShare
+            </h3>
+            <span> application</span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
+    </header>
+    
+  </div>
+
+  
+
+  <section class="login_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="detail-box">
+            <div>
+              <h3>
+                Welcome!
+              </h3>
+              <p>
+                Your account has been successfully created!
+              </p>
+              <a href="index.html"> Continue </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+
+  
+  <section class="info_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="info_menu">
+            <h5>
+              QUICK LINKS
+            </h5>
+            <ul class="navbar-nav  ">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html"> About </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="books.html"> Books </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html"> Contact Us </a>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info_course">
+          </div>
+        </div>
+
+        <div class="col-md-5 offset-md-1">
+          <div class="info_news">
+            <h5>
+              FOR ANY QUERY, PLEASE WRITE TO US
+            </h5>
+            <div class="info_contact">
+              <a href="https://cutt.ly/5mNGi66" target="_blank">
+                View Park Towers
+              </a>
+              <p>
+                Contact@NisaShare.com
+              </p>
+              <p>
+                Call : +254 1234567890
+              </p>
+            </div>
+            <form action="">
+              <p>Get Our Daily Newsletter!</p>
+              <input type="text" placeholder="Enter Your email" />
+              <button>
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+
+  
+  <footer class="container-fluid footer_section">
+    <p>
+      &copy; 2021 All Rights Reserved By NisaShare
+    </p>
+  </footer>
+
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+    )
+}
